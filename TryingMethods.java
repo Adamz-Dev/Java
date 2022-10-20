978278ba6ffd7eb714571c871ad94af94be33367
Adamz Muhammad <Adamzmuhammad21@gmail.com>
2022-10-20 13:03:10 +0100

    implement days of week dynamic method

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,1 +1,25 @@
 // Test Assignment class
+public class TryingMethods {
+    public static void main(String[] args) {
+
+    }
+
+    public static String daysOfWeek(int day) {
+        switch(day) {
+        case 1 :
+            return "Monday";
+        case 2 :
+            return "Tuesday";
+        case 3 :
+            return "Wednesday";
+        case 4 :
+            return "Thursday";
+        case 5 :
+            return "Friday";
+        default: 
+         return "put the correct Number of day";
+       
+        }
+    }
+
+}
